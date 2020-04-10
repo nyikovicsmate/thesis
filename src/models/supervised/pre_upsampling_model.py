e7f5680e1b3379bf06db95aa0cfc714d33f27d69
Nyikovics Máté <nyikovicsmate@gmail.com>
2020-04-10 03:46:46 +0200

    Add support for arbitrary sizes.

--- src/models/supervised/pre_upsampling_model.py
+++ src/models/supervised/pre_upsampling_model.py
@@ -3,14 +3,13 @@ import tensorflow as tf
 
 
 class PreUpsamplingModel(tf.keras.models.Model):
-    SCALE: int = 2
 
     def __init__(self,
-                 input_shape: Tuple[int, int, int]):    # (height, width, depth)
+                 scaling_factor: float = 2,
+                 input_shape: Tuple[int, int, int] = (None, None, 1)):    # (height, width, depth), arbitrary grayscale images as default
         super().__init__()
-        self._input_shape = input_shape
-        self._output_shape = (*map(lambda x: x * self.SCALE, self._input_shape[:2]), self._input_shape[2])
-        self.conv1 = tf.keras.layers.Conv2D(input_shape=self._output_shape,
+        self.scaling_factor = scaling_factor
+        self.conv1 = tf.keras.layers.Conv2D(input_shape=input_shape,
                                             filters=64,
                                             kernel_size=6,
                                             strides=1,
@@ -31,55 +30,25 @@ class PreUpsamplingModel(tf.keras.models.Model):
                                             activation="relu",
                                             kernel_initializer=None,
                                             bias_initializer=None)
-        # self.conv3 = tf.keras.layers.Conv2D(filters=128,
-        #                                     kernel_size=3,
-        #                                     strides=1,
-        #                                     padding="same",
-        #                                     data_format="channels_last",
-        #                                     use_bias=True,
-        #                                     dilation_rate=1,  # no dilation
-        #                                     activation="relu",
-        #                                     kernel_initializer=None,
-        #                                     bias_initializer=None)
-        # self.conv4 = tf.keras.layers.Conv2D(filters=128,
-        #                                     kernel_size=3,
-        #                                     strides=1,
-        #                                     padding="same",
-        #                                     data_format="channels_last",
-        #                                     use_bias=True,
-        #                                     dilation_rate=1,  # no dilation
-        #                                     activation="relu",
-        #                                     kernel_initializer=None,
-        #                                     bias_initializer=None)
-        # self.conv5 = tf.keras.layers.Conv2D(filters=128,
-        #                                     kernel_size=3,
-        #                                     strides=1,
-        #                                     padding="same",
-        #                                     data_format="channels_last",
-        #                                     use_bias=True,
-        #                                     dilation_rate=1,  # no dilation
-        #                                     activation="relu",
-        #                                     kernel_initializer=None,
-        #                                     bias_initializer=None)
-        self.conv6 = tf.keras.layers.Conv2D(filters=self._output_shape[2],
+        self.conv3 = tf.keras.layers.Conv2D(filters=input_shape[-1],    # output depth is the same as the input's depth
                                             kernel_size=3,
                                             strides=1,
                                             padding="same",
                                             data_format="channels_last",
                                             use_bias=True,
                                             dilation_rate=1,  # no dilation
-                                            activation="linear",
+                                            activation="linear",    # no activation
                                             kernel_initializer=None,
                                             bias_initializer=None)
 
     @tf.function
     def call(self, inputs, training=None, mask=None):
-        inputs = tf.image.resize(inputs, tuple(self._output_shape[:2]), tf.image.ResizeMethod.BICUBIC)
+        _shape = tf.shape(inputs)   # expecting 4D tensor in channel_last format
+        _new_size = (_shape[1] * self.scaling_factor, _shape[2] * self.scaling_factor)
+        # scale the inputs
+        inputs = tf.image.resize(inputs, _new_size, tf.image.ResizeMethod.BICUBIC)
         x = self.conv1(inputs)
         x = self.conv2(x)
-        # x = self.conv3(x)
-        # x = self.conv4(x)
-        # x = self.conv5(x)
-        outputs = self.conv6(x)
+        outputs = self.conv3(x)
         return outputs
 
