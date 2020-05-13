f317c003b227887c2e129630414d2af2aa40606c
Nyikovics Máté <nyikovicsmate@gmail.com>
2020-05-13 04:53:13 +0200

    Move `optimizer` and `learning_rate` function variables' instantiation into constructor.
    First `optimizer.appy_gradients()` call always instantiates new gradient tensors, which
    causes a problem when multiple subsequent `train` calls occur.
    For more info, see https://github.com/tensorflow/tensorflow/issues/27120

--- src/networks/supervised/progressive_upsampling_network.py
+++ src/networks/supervised/progressive_upsampling_network.py
@@ -17,6 +17,11 @@ class ProgressiveUpsamplingNetwork(Network):
     def __init__(self, input_shape: Tuple[Optional[int], Optional[int], Optional[int]] = (None, None, 1)):
         model = ProgressiveUpsamplingModel(input_shape=input_shape)
         super().__init__(model)
+        self.learning_rate = tf.Variable(tf.constant(0, dtype=tf.float32))
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate,
+                                                  beta_1=0.9,
+                                                  beta_2=0.999,
+                                                  epsilon=1e-8)
 
     @staticmethod
     @tf.function
@@ -44,28 +49,24 @@ class ProgressiveUpsamplingNetwork(Network):
         LOGGER.warn(f"Couldn't predict.")
 
     @tf.function
-    def _train_step(self, x, y, optimizer):
+    def _train_step(self, x, y):
         with tf.GradientTape() as tape:
             y_pred = self.model(x)
             loss = ProgressiveUpsamplingNetwork.custom_loss(y, y_pred)
             grads = tape.gradient(loss, self.model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
         return loss
 
     def _train(self, x, y, loss_func, epochs, learning_rate, callbacks):
         assert loss_func is None, "Reinforced network uses it's own loss function. Pass it `None`"
-        learning_rate = tf.Variable(learning_rate)      # wrap variable according to callbacks.py:25
-        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate,
-                                             beta_1=0.9,
-                                             beta_2=0.999,
-                                             epsilon=1e-8)
+        self.learning_rate.assign(tf.constant(learning_rate, dtype=tf.float32))
         # train
         for e_idx in range(epochs):
             train_loss = tf.constant(0, dtype=tf.float32)
             start_sec = time.time()
             # process a batch
             for x_b, *y_b in zip_longest(x, *y):
-                train_loss += self._train_step(x_b, y_b, optimizer)
+                train_loss += self._train_step(x_b, y_b)
             # update state
             delta_sec = time.time() - start_sec
             self.state.epochs += 1
@@ -76,6 +77,6 @@ class ProgressiveUpsamplingNetwork(Network):
                 # manually update learning rate and call iteration end callbacks
                 for cb in callbacks:
                     if isinstance(cb, OptimizerCallback):
-                        learning_rate.assign(cb(self))
+                        self.learning_rate.assign(tf.constant(cb(self), dtype=tf.float32))
                     if isinstance(cb, TrainIterationEndCallback):
                         cb(self)
